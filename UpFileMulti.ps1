67b929f2e9c144dfe5fc85dddac8377b5da2e99d
ayhamzz93 <148772964+ayhamzz93@users.noreply.github.com>
2025-10-16 08:29:11 +0200

    Update UpFileMulti.ps1

--- UpFileMulti.ps1
+++ UpFileMulti.ps1
@@ -113,7 +113,7 @@ $deviceInfo =  $deviceInfo1 | ConvertTo-Json
 
 Write-Host "Trigger Webhook..." -ForegroundColor Blue
 #Trigger Webhook
-$uri = 'https://c12c6805-7e33-44ea-a095-2105f519a048.webhook.sec.azure-automation.net/webhooks?token=dAAYVzr2uP1xj7ZhxjZV0njrT%2bPmEK60vJRKEnWUveE%3d'
+$uri = 'https://c12c6805-7e33-44ea-a095-2105f519a048.webhook.sec.azure-automation.net/webhooks?token=dAAYVzr2uP1xj7ZhxjZV0njrT%2bPmEK60vJRKEnWUveE%3dd'
 $body = $deviceInfo
 $header = @{"Content-Type" = "application/json"}
 $response = Invoke-WebRequest -Method Post -Uri $uri -Body $body -Headers $header -UseBasicParsing
@@ -121,5 +121,6 @@ $jobid = (ConvertFrom-Json ($response.Content)).jobids[0]
 Write-Host "JobID: $jobid" -ForegroundColor Yellow
 Write-Host "
 Task Completed." -ForegroundColor Blue
+
 
 
